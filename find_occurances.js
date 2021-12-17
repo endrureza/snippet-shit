d8f2c31c83ad8f1724de2d66a41f8e69645e048b
Endru Reza <lotusb13@gmail.com>
2021-12-17 15:33:04 +0700

    chore: add another solution to find_occurances

--- find_occurances.js
+++ find_occurances.js
@@ -1,37 +1,52 @@
 const findTotalOccurancesEachCharInAString = (str) => {
-    let charMap = {};
-    let merge = '';
+  let charMap = {};
+  let merge = "";
 
-    for (let char of str) {
-        if (charMap[char]) {
-            charMap[char]++;
-        } else {
-            charMap[char] = 1;
-        }
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
     }
+  }
 
-    for (let char in charMap) {
-        merge += char + charMap[char];
-    }
-    
-    return merge;
-}
+  for (let char in charMap) {
+    merge += char + charMap[char];
+  }
+
+  return merge;
+};
 
 const findTotalOccurancesEachCharInAStringV2 = (str) => {
-    let charMap = {};
+  let charMap = {};
 
-    for (let char of str) {
-        if (charMap[char]) {
-            charMap[char]++;
-        } else {
-            charMap[char] = 1;
-        }
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
     }
+  }
 
-    return Object.keys(charMap).filter((res) => charMap[res] > 1).join('');
-}
+  return Object.keys(charMap)
+    .filter((res) => charMap[res] > 1)
+    .join("");
+};
+
+const findTotalOccurancesEachCharInAStringV3 = (str) => {
+  let charMap = {};
+
+  str.split("").forEach((char) => {
+    charMap[char] = (charMap[char] || 0) + 1;
+  });
+
+  return Object.keys(charMap)
+    .filter((res) => charMap[res] > 1)
+    .join("");
+};
 
 export {
-    findTotalOccurancesEachCharInAString,
-    findTotalOccurancesEachCharInAStringV2,
-}
+  findTotalOccurancesEachCharInAString,
+  findTotalOccurancesEachCharInAStringV2,
+  findTotalOccurancesEachCharInAStringV3,
+};
